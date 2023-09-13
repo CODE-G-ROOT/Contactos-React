7a01137feb582c9096954e0703e47d49edd6ed65
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-09-13 17:03:13 -0500

    feat: :heavy_plus_sign: Dependencias de las rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,33 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import Root from "./routes/root";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import "./index.css";
+import ErrorPage from "./error-page";
+import Contact, { loader as contactLoader } from "./routes/contact";
+import EditContact, {action as editAction} from "./routes/edit";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element:  <Root />,
-  },
+    errorElement: <ErrorPage/>,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact/>,
+        loader: contactLoader,
+        action: editAction,
+      }
+    ],
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,18 @@
+import { Outlet, Link, useLoaderData, Form, redirect, NavLink } from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
+
+export async function action() {
+    const contact = await createContact();
+    return redirect(`/contacts/${contact.id}/edit`);
+};
+
+export async function loader() {
+    const contacts = await getContacts();
+    return { contacts };
+};
+
 export default function Root() {
+    const { contacts } = useLoaderData();
     return (
         <>
             <div id="sidebar">
@@ -22,22 +36,50 @@ export default function Root() {
                             aria-live="polite"
                         ></div>
                     </form>
-                    <form method="post">
+                    <Form method="post">
                         <button type="submit">New</button>
-                    </form>
+                    </Form>
                 </div>
                 <nav>
-                    <ul>
-                        <li>
-                            <a href={`/contacts/1`}>Your Name</a>
-                        </li>
-                        <li>
-                            <a href={`/contacts/2`}>Your Friend</a>
-                        </li>
-                    </ul>
+                    {contacts.length ? (
+                        <ul>
+                            {contacts.map((contact) => (
+                                <li key={contact.id}>
+                                    <NavLink
+                                        to={`contacts/${contact.id}`}
+                                        className={({ isActive, isPending }) =>
+                                            isActive
+                                                ? "active"
+                                                : isPending
+                                                    ? "pending"
+                                                    : ""
+                                        }
+                                    >
+                                        <Link to={`contacts/${contact.id}`}>
+                                        {contact.first || contact.last ? (
+                                            <>
+                                                {contact.first} {contact.last}
+                                            </>
+                                        ) : (
+                                            <i>No Name</i>
+                                        )}{" "}
+                                        {contact.favorite && <span>★</span>}
+                                    </Link>
+                                    </NavLink>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>
+                            <i>No contacts</i>
+                        </p>
+                    )}
                 </nav>
             </div>
             <div id="detail"></div>
+            <div id="detail">
+                <Outlet />
+            </div>
         </>
     );
 }
