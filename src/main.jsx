adfefb68c772a98353400f060bfc78b497558354
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-09-13 13:21:02 -0500

    feat: :sparkles: Implenetación de las configuraciónes
    se importaron las configuración de los archivos root

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
-import './index.css';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import Root from "./routes/root";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div></div>
-  }
-])
+    element:  <Root />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
