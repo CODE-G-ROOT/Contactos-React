4d928e9d76adf69fac4db2f578d930b2eb6273ec
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-09-13 09:25:07 -0500

    feat: :truck: Enrutador

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div></div>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
